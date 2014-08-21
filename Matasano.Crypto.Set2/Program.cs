40fb5dd33cb4cdad8a315afb2eb5ed5b5889465e
Entroper <entroper@gmail.com>
2014-08-21 16:59:59 -0400

    9 and 10

---
+++ Matasano.Crypto.Set2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matasano.Crypto.Set2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//Problem9();
+			Problem10();
+		}
+
+		public static void Problem9()
+		{
+			Console.WriteLine(Crypto.AddPkcs7Padding("YELLOW SUBMARINE".ToBytes(), 20).ToText());
+		}
+
+		public static void Problem10()
+		{
+			byte[] cipherBytes = ByteUtilities.Base64FileToBytes("10.txt");
+			byte[] plainBytes = Crypto.Aes128CbcDecrypt(cipherBytes, "YELLOW SUBMARINE".ToBytes(), new byte[16]);
+			plainBytes = Crypto.RemovePkcs7Padding(plainBytes, 16);
+
+			Console.WriteLine(plainBytes.ToText());
+		}
+	}
+}
--- Matasano.Crypto/Crypto.cs
+++ Matasano.Crypto/Crypto.cs
@@ -10,6 +10,54 @@ namespace Matasano.Crypto
 {
 	public static class Crypto
 	{
+		public static byte[] Aes128EcbEncrypt(byte[] plainBytes, byte[] key)
+		{
+			var aes = new AesCryptoServiceProvider
+			{
+				KeySize = 128,
+				Key = key,
+				Mode = CipherMode.ECB,
+				Padding = PaddingMode.Zeros
+			};
+
+			var encryptor = aes.CreateEncryptor();
+			byte[] cipherBytes = new byte[plainBytes.Length];
+			for (int offset = 0; offset < plainBytes.Length; offset += aes.BlockSize/8)
+			{
+				encryptor.TransformBlock(plainBytes, offset, aes.BlockSize/8, cipherBytes, offset);
+			}
+
+			return cipherBytes;
+		}
+
+		public static byte[] Aes128CbcEncrypt(byte[] plainBytes, byte[] key, byte[] iv)
+		{
+			var aes = new AesCryptoServiceProvider
+			{
+				KeySize = 128,
+				Key = key,
+				Mode = CipherMode.ECB,
+				Padding = PaddingMode.Zeros
+			};
+
+			if (iv.Length != aes.BlockSize/8)
+				throw new InvalidOperationException("IV must be exactly one block length");
+
+			var encryptor = aes.CreateEncryptor();
+			byte[] cipherBytes = new byte[plainBytes.Length];
+			
+			byte[] block = new byte[16];
+			for (int offset = 0; offset < plainBytes.Length; offset += aes.BlockSize/8)
+			{
+				Buffer.BlockCopy(plainBytes, 0, block, 0, aes.BlockSize/8);
+				block = block.Xor(iv);
+				encryptor.TransformBlock(block, 0, aes.BlockSize/8, cipherBytes, offset);
+				Buffer.BlockCopy(cipherBytes, offset, iv, 0, aes.BlockSize/8);
+			}
+
+			return cipherBytes;
+		}
+
 		public static byte[] Aes128EcbDecrypt(byte[] cipherBytes, byte[] key)
 		{
 			var aes = new AesCryptoServiceProvider
@@ -28,6 +76,27 @@ namespace Matasano.Crypto
 			}
 
 			return plainBytes;
+		}
+
+		public static byte[] Aes128CbcDecrypt(byte[] cipherBytes, byte[] key, byte[] iv)
+		{
+			byte[] plainBytes = Aes128EcbDecrypt(cipherBytes, key);
+			plainBytes = plainBytes.Xor(iv.Concat(cipherBytes).ToArray());
+
+			return plainBytes;
+		}
+
+		public static byte[] AddPkcs7Padding(byte[] unpadded, int blockSize)
+		{
+			byte padding = (byte)(blockSize - unpadded.Length%blockSize);
+
+			byte[] padded = new byte[unpadded.Length + padding];
+			for (int i = unpadded.Length; i < padded.Length; i++)
+				padded[i] = padding;
+
+			Buffer.BlockCopy(unpadded, 0, padded, 0, unpadded.Length);
+
+			return padded;
 		}
 
 		public static byte[] RemovePkcs7Padding(byte[] padded, int blockSize)
